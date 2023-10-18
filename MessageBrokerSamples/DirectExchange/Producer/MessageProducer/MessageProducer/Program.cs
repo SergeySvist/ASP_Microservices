33cb7c679b4eb3b44d7e2ab3f793e415cb4548e7
Sergey Svist <sergeysvist557@gmail.com>
2023-10-18 13:18:56 +0300

    add: message producer

---
+++ MessageBrokerSamples/DirectExchange/Producer/MessageProducer/MessageProducer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+string firstMessageQueueName = "first.message.queue";
+string secondMessageQueueName = "second.message.queue";
+
+ConnectionFactory factory = new ConnectionFactory { HostName = "localhost" };
+
+using (IConnection connection = factory.CreateConnection())
+{
+    using (IModel channel = connection.CreateModel())
+    {
+        channel.QueueDeclare(
+            queue: firstMessageQueueName,
+            durable: true,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+            );
+
+        byte[] binaryMessageBody = Encoding.Unicode.GetBytes("Message #1. Hello buddy. What's up");
+
+        channel.BasicPublish(
+            exchange: string.Empty,
+            routingKey: firstMessageQueueName,
+            mandatory: false,
+            basicProperties: null,
+            body: binaryMessageBody
+            );
+    }
+}
